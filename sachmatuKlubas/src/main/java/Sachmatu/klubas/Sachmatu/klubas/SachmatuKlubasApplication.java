94b676a60a5754b0c447fa9ac185c6b03554c5f2
Aleksas Briliovas <aleksas.briliovasc1@gmail.com>
2023-03-07 20:26:10 +0200

    first commit created the spring project

---
+++ sachmatuKlubas/src/main/java/Sachmatu/klubas/Sachmatu/klubas/SachmatuKlubasApplication.java
@@ -0,0 +1,13 @@
+package Sachmatu.klubas.Sachmatu.klubas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SachmatuKlubasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SachmatuKlubasApplication.class, args);
+	}
+
+}
---
+++ sachmatuKlubas/src/test/java/Sachmatu/klubas/Sachmatu/klubas/SachmatuKlubasApplicationTests.java
@@ -0,0 +1,13 @@
+package Sachmatu.klubas.Sachmatu.klubas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SachmatuKlubasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
